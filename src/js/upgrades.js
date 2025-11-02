c89ed0878a29ab5c34a8bd17cc5b1268b17ea016
Jason <jasn74939@gmail.com>
2025-11-02 15:04:48 -0500

    bug fixes in upgrades

--- src/js/upgrades.js
+++ src/js/upgrades.js
@@ -1,33 +1,36 @@
 // wood pick
 wood_pick_upgrade.addEventListener('click', () => {
-    if (gold_count >= 50 && wood_pick_status === 'not_purchased') {
-        gold_count -= 50;
+    if (cash_count >= 100 && wood_pick_status === 'not_purchased') {
+        cash_count -= 100;
         multiplier += 2;
         wood_pick_status = 'purchased';
-        document.getElementById("gold_count_output").innerHTML = `<p>You have: ${gold_count}</p>`;
+        document.getElementById("cash_count_output").innerHTML = `<p>You have: ${cash_count}</p>`;
     }
 });
 // stone pick
 stone_pick_upgrade.addEventListener('click', async () => {
-    if (gold_count >= 200 && stone_pick_status === 'not_purchased') {
+    if (cash_count >= 500 && stone_pick_status === 'not_purchased') {
+        cash_count -= 500;
         multiplier += 3;
         stone_pick_status = 'purchased';
-        document.getElementById("gold_count_output").innerHTML = `<p>You have: ${gold_count}</p>`;
+        document.getElementById("cash_count_output").innerHTML = `<p>You have: ${cash_count}</p>`;
     }
 });
 // copper pick
 copper_pick_upgrade.addEventListener('click', async () => {
-    if (gold_count >= 400 && copper_pick_status === 'not_purchased') {
+    if (cash_count >= 1250 && copper_pick_status === 'not_purchased') {
+        cash_count -= 1250;
         multiplier += 5;
         copper_pick_status = 'purchased';
-        document.getElementById("gold_count_output").innerHTML = `<p>You have: ${gold_count}</p>`;
+        document.getElementById("cash_count_output").innerHTML = `<p>You have: ${cash_count}</p>`;
     }
 });
 // iron pick
 iron_pick_upgrade.addEventListener('click', async () => {
-    if (gold_count >= 400 && copper_pick_status === 'not_purchased') {
+    if (cash_count >= 2000 && copper_pick_status === 'not_purchased') {
+        cash_count -= 2000;
         multiplier += 6;
         iron_pick_status = 'purchased';
-        document.getElementById("gold_count_output").innerHTML = `<p>You have: ${gold_count}</p>`;
+        document.getElementById("cash_count_output").innerHTML = `<p>You have: ${cash_count}</p>`;
     }
 });
