ced48d8587cfbff035d524bd3ad00787d53edf1f
Jason <jasn74939@gmail.com>
2025-11-02 16:05:14 -0500

    add more pick upgrades, and restructure html

--- src/js/state.js
+++ src/js/state.js
@@ -5,6 +5,10 @@ const wood_pick_upgrade = document.getElementById('wood_pick_upgrade');
 const stone_pick_upgrade = document.getElementById('stone_pick_upgrade');
 const copper_pick_upgrade = document.getElementById('copper_pick_upgrade');
 const iron_pick_upgrade = document.getElementById('iron_pick_upgrade');
+const gold_pick_upgrade = document.getElementById('gold_pick_upgrade');
+const emerald_pick_upgrade = document.getElementById('emerald_pick_upgrade');
+const diamond_pick_upgrade = document.getElementById('diamond_pick_upgrade');
+
 
 let gold_count = 0;
 let coal_count = 0;
@@ -14,3 +18,6 @@ let wood_pick_status = 'not_purchased';
 let stone_pick_status = 'not_purchased';
 let copper_pick_status = 'not_purchased';
 let iron_pick_status = 'not_purchased';
+let gold_pick_status = 'not_purchased';
+let emerald_pick_status = 'not_purchased';
+let diamond_pick_status = 'not_purchased';
--- src/js/upgrades.js
+++ src/js/upgrades.js
@@ -34,3 +34,30 @@ iron_pick_upgrade.addEventListener('click', async () => {
         document.getElementById("cash_count_output").innerHTML = `<p>You have: ${cash_count}</p>`;
     }
 });
+// gold pick upgrade
+gold_pick_upgrade.addEventListener('click', async () => {
+    if (cash_count >= 2500 && gold_pick_status === 'not_purchased') {
+        cash_count -= 2500;
+        multiplier += 7;
+        gold_pick_status = 'purchased';
+        document.getElementById("cash_count_output").innerHTML = `<p>You have: ${cash_count}</p>`;
+    }
+});
+// emerald pick upgrade
+emerald_pick_upgrade.addEventListener('click', async () => {
+    if (cash_count >= 3200 && emerald_pick_status === 'not_purchased') {
+        cash_count -= 3200;
+        multiplier += 9;
+        emerald_pick_status = 'purchased';
+        document.getElementById("cash_count_output").innerHTML = `<p>You have: ${cash_count}</p>`;
+    }
+});
+// diamond pick upgrade
+diamond_pick_upgrade.addEventListener('click', async () => {
+    if (cash_count >= 5000 && diamond_pick_status === 'not_purchased') {
+        cash_count -= 5000;
+        multiplier += 12;
+        diamond_pick_status = 'purchased';
+        document.getElementById("cash_count_output").innerHTML = `<p>You have: ${cash_count}</p>`;
+    }
+});
